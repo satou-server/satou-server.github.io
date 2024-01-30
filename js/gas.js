e23c813751a7265f8b4bc159993b7f488cca00ac
YOWASHI <157606941+satou-server@users.noreply.github.com>
2024-01-31 02:42:29 +0900

    Update gas.js

--- js/gas.js
+++ js/gas.js
@@ -2,6 +2,14 @@ const gas_url = 'https://script.google.com/macros/s/AKfycbzdj-WPE4zVcn5MTohDfMAc
 
 fetch(gas_url)
 .then(function (data) {
+    const serverMember_p    = document.getElementById("server-members");
+    const serberMember_span = serverMember_p.querySelector("span");
+    serberMember_span.textContent = data.server_member;
+
+    const activeMember_p    = document.getElementById("active-members");
+    const activeMember_span = serverMember_p.querySelector("span");
+    activeMember_span.textContent = data.active_member;
+
     console.log(data.server_member);
     console.log(data.active_member);
 });
