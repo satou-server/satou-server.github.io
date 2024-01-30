6dc03891dc2eebadb5fe7009f72db0c95a424980
YOWASHI <157606941+satou-server@users.noreply.github.com>
2024-01-31 02:34:31 +0900

    Update gas.js

--- js/gas.js
+++ js/gas.js
@@ -1,15 +1,7 @@
-const googleAppsScriptUrl = 'https://script.google.com/macros/s/AKfycbzdj-WPE4zVcn5MTohDfMAc53zKUm8RtSKbVYU78dmIADWjC3wbh-ZZBQ5LtEdL6xn7/exec';
+const gas_url = 'https://script.google.com/macros/s/AKfycbzdj-WPE4zVcn5MTohDfMAc53zKUm8RtSKbVYU78dmIADWjC3wbh-ZZBQ5LtEdL6xn7/exec';
 
-// ランダムなコールバック関数名を生成
-const callbackName = 'callback_' + Math.floor(Math.random() * 10000);
-
-// グローバルスコープにコールバック関数を定義
-window[callbackName] = function(data) {
+fetch(gas_url)
+.then(function (data) {
     console.log(data.server_member);
     console.log(data.active_member);
-};
-
-// JSONPリクエストを発行
-const script = document.createElement('script');
-script.src = `${googleAppsScriptUrl}?callback=${callbackName}`;
-document.head.appendChild(script);
+});
