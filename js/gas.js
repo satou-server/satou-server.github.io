725aaa4125b12bcb09eacbc3e68af508417037bc
YOWASHI <157606941+satou-server@users.noreply.github.com>
2024-02-02 22:06:34 +0900

    Add files via upload

--- js/gas.js
+++ js/gas.js
@@ -1,15 +1,19 @@
-const gas_url = 'https://script.google.com/macros/s/AKfycbzdj-WPE4zVcn5MTohDfMAc53zKUm8RtSKbVYU78dmIADWjC3wbh-ZZBQ5LtEdL6xn7/exec';
+const gas_url = 'https://script.google.com/macros/s/AKfycbwBlE1FEr1EhjNmEdZdSGFVO1ZMU8Vrg44yWTL3d7QebnFI921Xlj3gK-G9rb3lWOgg/exec';
 
 fetch(gas_url)
-.then(function (data) {
-    const serverMember_p    = document.getElementById("server-members");
-    const serberMember_span = serverMember_p.querySelector("span");
-    serberMember_span.textContent = data.server_member;
+    .then(response => response.json())
+    .then(data => {
+        const serverMember_p    = document.getElementById("server-members");
+        const serverMember_span = serverMember_p.querySelector("span");
+        serverMember_span.textContent = data.server_member;
 
-    const activeMember_p    = document.getElementById("active-members");
-    const activeMember_span = serverMember_p.querySelector("span");
-    activeMember_span.textContent = data.active_member;
+        const activeMember_p    = document.getElementById("active-members");
+        const activeMember_span = activeMember_p.querySelector("span");
+        activeMember_span.textContent = data.active_member;
 
-    console.log(data.server_member);
-    console.log(data.active_member);
-});
+        console.log(data.server_member);
+        console.log(data.active_member);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
