053ea6168b6f7ff0ff15770e407d9394a38f6593
YOWASHI <157606941+satou-server@users.noreply.github.com>
2024-02-03 21:02:00 +0900

    Add files via upload

--- js/gas.js
+++ js/gas.js
@@ -1,18 +1,19 @@
-const gas_url = 'https://script.google.com/macros/s/AKfycbwBlE1FEr1EhjNmEdZdSGFVO1ZMU8Vrg44yWTL3d7QebnFI921Xlj3gK-G9rb3lWOgg/exec';
+const gas_url = 'https://script.google.com/macros/s/AKfycbzg0mkjf0CrfOwByiw1lSq9FjHS6fivdxcAAaLAFBjsTwDZ1V0nVn-Xhv_gTIIllE02/exec';
 
 fetch(gas_url)
     .then(response => response.json())
     .then(data => {
-        const serverMember_p    = document.getElementById("server-members");
-        const serverMember_span = serverMember_p.querySelector("span");
+        const serverMember_p    = document.getElementById('server-members');
+        const serverMember_span = serverMember_p.querySelector('span');
         serverMember_span.textContent = data.server_member;
 
-        const activeMember_p    = document.getElementById("active-members");
-        const activeMember_span = activeMember_p.querySelector("span");
+        const activeMember_p    = document.getElementById('server-actives');
+        const activeMember_span = activeMember_p.querySelector('span');
         activeMember_span.textContent = data.active_member;
 
-        console.log(data.server_member);
-        console.log(data.active_member);
+        const serverBoost_p     = document.getElementById('server-boost');
+        const serverBoost_span  = serverBoost_p.querySelector('span');
+        serverBoost_span.textContent = data.boost_level;
     })
     .catch(error => {
         console.error('Fetch error:', error);
