03af55fe73f68dcdd7cc672ebb15c40e6801610c
YOWASHI <157606941+satou-server@users.noreply.github.com>
2024-02-03 21:02:14 +0900

    Delete js/html directory

--- js/html/main.js
+++
@@ -1,9 +0,0 @@
-
-(function () {
-    // main
-    let main=document.createElement('main');
-    document.body.appendChild(main);
-    main=document.querySelector('main')
-    // test-p
-    let 
-}());
